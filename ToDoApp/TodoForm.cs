4ac2cba3fed5c0d077d2cfd8032d76fc3484e428
Aleksandr <aleksstanov1984@gmail.com>
2025-04-25 18:27:26 +0200

    added a timer that updates task statuses
    Add TodoStatusChecker and improve TodoForm comments
    
    Updated TodoForm.cs to include Todo.TimerToDo and added a
    private readonly TodoStatusChecker field. Enhanced comments
    for clarity, commented out unused RoundedButton code, and
    introduced TodoForm_Load to start the checker. Improved
    UpdateToDoList method comments and readability of priority
    counter updates.

--- ToDoApp/TodoForm.cs
+++ ToDoApp/TodoForm.cs
@@ -4,56 +4,79 @@ using ToDo = Todo.ToDo;
 using TodoItem = DbLibrary.Models.TodoItem;
 using ToDoApp.Forms;
 using Todo.Enums;
+using Todo.TimerToDo;
 
 namespace ToDoApp
 {
     public partial class TodoForm : Form
     {
-        public ToDo todo = new(); // Создаем экземпляр класса Todo
+        // Создаем экземпляр класса Todo
+        public ToDo todo = new(); 
+
+        // Создаем экземпляр класса TodoStatusChecker
+        private readonly TodoStatusChecker checker = new();
 
         public TodoForm()
         {
-            InitializeComponent();
+            // Инициализируем компоненты формы
+            InitializeComponent(); 
 
-            UpdateToDoList();
+            // 
+            UpdateToDoList(); 
 
-            //RoundedButton button1 = new RoundedButton
-            //{
-            //    Location = new Point(30, 50),
-            //    Name = "button1",
-            //    Size = new Size(100, 50),
-            //    TabIndex = 1,
-            //    Text = "Круглая кнопка",
-            //    UseVisualStyleBackColor = true,
-            //    BackColor = Color.LightBlue,
-            //    BorderRadius = 25 // Радиус скругления
-            //};
+            /* RoundedButton button1 = new RoundedButton
+            {
+                Location = new Point(30, 50),
+                Name = "button1",
+                Size = new Size(100, 50),
+                TabIndex = 1,
+                Text = "Круглая кнопка",
+                UseVisualStyleBackColor = true,
+                BackColor = Color.LightBlue,
+                BorderRadius = 25 // Радиус скругления
+            };
 
-            //this.Controls.Add(button1);
+            this.Controls.Add(button1);
+            */
         }
 
+        // 
+        public void TodoForm_Load(object sender, EventArgs e)
+        {
+            checker.Start();
+        }
+
+        // 
         private void BtnCreateNewToDo_Click(object sender, EventArgs e)
         {
             CreateOrUpdateToDo_Form createNewToDo = new(Convert.ToBoolean(((Button)sender).TabIndex));
+
             createNewToDo.ShowDialog();
 
+            // Обновляем список задач после создания новой задачи
             UpdateToDoList();
         }
 
+        // 
         private void BtnUpdateToDo_Click(object sender, EventArgs e)
         {
+            // Создаем экземпляр класса UpdateToDo_Form
             UpdateToDo_Form createNewToDo = new(((Button)sender).TabIndex);
+
             createNewToDo.ShowDialog();
         }
 
         public void UpdateToDoList()
         {
+            // Получаем все задачи из базы данных
             List<TodoItem> newAllTodos = todo.GetNewToDoList();
 
+            // Получаем все задачи из базы данных
             var countsByPriority = newAllTodos
                 .GroupBy(todo => (PriorityToDo)todo.Priority) // Explicitly cast 'int' to 'TaskStatusToDo'
                 .ToDictionary(g => g.Key, g => g.Count());
 
+            // Обновляем счетчики приоритетов
             foreach ((PriorityToDo priority, int count) in countsByPriority)
             {
                 string priorityText = priority switch
@@ -66,7 +89,6 @@ namespace ToDoApp
                 };
 
                 Console.WriteLine(priorityText);
-
             }
         }
     }
